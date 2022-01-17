5320514fa091b7d45c68dab3c6c0539257c8ce31
Redavela <kodadreda67@gmail.com>
2022-01-17 12:48:42 +0100

    add button scroll top

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 
-
+let myButton = document.getElementById("myBtn");
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
 const allLinks = document.querySelectorAll('.item-nav');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 function toggleButton() {
     navList.classList.toggle('show');
